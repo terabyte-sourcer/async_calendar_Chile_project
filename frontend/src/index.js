5633b7d011a315efee6dce926b3f7117145e7dd2
chenglai <chenglai.taya@gmail.com>
2025-03-16 20:01:23 -0400

    react-flag debug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,6 +7,14 @@ import App from './App';
 import theme from './theme';
 import './index.css';
 
+// Configure future flags for React Router
+// This will be picked up by React Router v6
+window.history.scrollRestoration = 'manual';
+window.__reactRouterFutureFlags = {
+    v7_startTransition: true,
+    v7_relativeSplatPath: true
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
